450b4a5541ba84ec4dab19fa0de9a0a0c612343e
whdlgp <whdlgp@gmail.com>
2019-04-22 22:00:55 +0900

    add test with image, reorder code (fc and cnn)

--- mnist_test.py
+++ mnist_test.py
@@ -27,60 +27,32 @@ CLASS_NUM = 10
 EPOCH_NUM = 10
 BATCH_NUM = 1000
 
-(x_train_cnn, x_test_cnn, y_train_cnn, y_test_cnn, input_shape_cnn) =  mnist_test.read_dataset_cnn(CLASS_NUM)
+# custom image dataset
+(x_image_test_dense, y_image_test_dense, input_shape_test_dense) = mnist_test.read_dataset_image_dense(CLASS_NUM)
+(x_image_test_cnn, y_image_test_cnn, input_shape_test_cnn) = mnist_test.read_dataset_image_cnn(CLASS_NUM)
+
+# load from mnist dataset
 (x_train_dense, x_test_dense, y_train_dense, y_test_dense, input_shape_dense) =  mnist_test.read_dataset_dense(CLASS_NUM)
+(x_train_cnn, x_test_cnn, y_train_cnn, y_test_cnn, input_shape_cnn) =  mnist_test.read_dataset_cnn(CLASS_NUM)
 
 # models list for test
-model_list_cnn = []
 model_list_dense = []
-# mnist with CNN, drop out\
-model_list_cnn_name = ['CNN_nodropout', 'CNN_dropout']
-model_list_cnn.append(mnist_test.mnist_cnn_model(input_shape_cnn, CLASS_NUM))
-model_list_cnn.append(mnist_test.mnist_cnn_dropout_model(input_shape_cnn, CLASS_NUM))
+model_list_cnn = []
 # mnist with fully connected network, drop out
 model_list_dense_name = ['FC_nodropout', 'FC_dropout']
 model_list_dense.append(mnist_test.mnist_dense_model(input_shape_dense, CLASS_NUM))
 model_list_dense.append(mnist_test.mnist_dense_dropout_model(input_shape_dense, CLASS_NUM))
+# mnist with CNN, drop out
+model_list_cnn_name = ['CNN_nodropout', 'CNN_dropout']
+model_list_cnn.append(mnist_test.mnist_cnn_model(input_shape_cnn, CLASS_NUM))
+model_list_cnn.append(mnist_test.mnist_cnn_dropout_model(input_shape_cnn, CLASS_NUM))
 
-# Learn CNN and FC
-
-print('Test CNN and CNN with drop out')
-for i, model in enumerate(model_list_cnn):
-    # draw model, save
-    plot_model(model, to_file=model_list_cnn_name[i]+'_model.png', show_shapes=True, show_layer_names=True)
-
-    # Learning
-    model.compile(loss=keras.losses.categorical_crossentropy,
-                optimizer=keras.optimizers.Adadelta(),
-                metrics=['accuracy'])
-    history = model.fit(x_train_cnn, y_train_cnn,
-                        batch_size=BATCH_NUM,
-                        epochs=EPOCH_NUM,
-                        verbose=1,
-                        validation_data=(x_test_cnn, y_test_cnn))
-    score = model.evaluate(x_test_cnn, y_test_cnn, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
-
-    # draw loss, save
-    y_vloss = history.history['val_loss']
-    y_loss = history.history['loss']
-
-    x_len = np.arange(len(y_loss))
-    plt.plot(x_len, y_vloss, marker='.', c='red', label="Validation-set Loss")
-    plt.plot(x_len, y_loss, marker='.', c='blue', label="Train-set Loss")
-
-    plt.legend(loc='upper right')
-    plt.grid()
-    plt.xlabel('epoch')
-    plt.ylabel('loss')
-    plt.savefig(model_list_cnn_name[i]+'_loss.png')
-    plt.cla()
+# Learn FC and CNN
 
 print('Test FC network and FC with drop out')
 for i, model in enumerate(model_list_dense):
     # draw model, save
-    plot_model(model, to_file=model_list_dense_name[i]+'_model.png', show_shapes=True, show_layer_names=True)
+    plot_model(model, to_file='test_results/'+model_list_dense_name[i]+'_model.png', show_shapes=True, show_layer_names=True)
 
     # Learning
     model.compile(loss=keras.losses.categorical_crossentropy,
@@ -91,9 +63,6 @@ for i, model in enumerate(model_list_dense):
                         epochs=EPOCH_NUM,
                         verbose=1,
                         validation_data=(x_test_dense, y_test_dense))
-    score = model.evaluate(x_test_dense, y_test_dense, verbose=0)
-    print('Test loss:', score[0])
-    print('Test accuracy:', score[1])
 
     # draw loss, save
     y_vloss = history.history['val_loss']
@@ -107,5 +76,45 @@ for i, model in enumerate(model_list_dense):
     plt.grid()
     plt.xlabel('epoch')
     plt.ylabel('loss')
-    plt.savefig(model_list_dense_name[i]+'_loss.png')
+    plt.savefig('test_results/'+model_list_dense_name[i]+'_loss.png')
     plt.cla()
+    
+    score = model.evaluate(x_image_test_dense, y_image_test_dense, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    print('')
+
+print('Test CNN and CNN with drop out')
+for i, model in enumerate(model_list_cnn):
+    # draw model, save
+    plot_model(model, to_file='test_results/'+model_list_cnn_name[i]+'_model.png', show_shapes=True, show_layer_names=True)
+
+    # Learning
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                optimizer=keras.optimizers.Adadelta(),
+                metrics=['accuracy'])
+    history = model.fit(x_train_cnn, y_train_cnn,
+                        batch_size=BATCH_NUM,
+                        epochs=EPOCH_NUM,
+                        verbose=1,
+                        validation_data=(x_test_cnn, y_test_cnn))
+
+    # draw loss, save
+    y_vloss = history.history['val_loss']
+    y_loss = history.history['loss']
+
+    x_len = np.arange(len(y_loss))
+    plt.plot(x_len, y_vloss, marker='.', c='red', label="Validation-set Loss")
+    plt.plot(x_len, y_loss, marker='.', c='blue', label="Train-set Loss")
+
+    plt.legend(loc='upper right')
+    plt.grid()
+    plt.xlabel('epoch')
+    plt.ylabel('loss')
+    plt.savefig('test_results/'+model_list_cnn_name[i]+'_loss.png')
+    plt.cla()
+
+    score = model.evaluate(x_image_test_cnn, y_image_test_cnn, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
+    print('')
--- mnist_test_model.py
+++ mnist_test_model.py
@@ -7,6 +7,14 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
+# import to read images
+from skimage import io
+from skimage import util 
+import numpy as np
+import os
+from os import path
+import matplotlib.pyplot as plt
+
 def read_dataset_cnn(CLASS_NUM):
     # Load mnist data set
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -63,6 +71,69 @@ def read_dataset_dense(CLASS_NUM):
     y_test = keras.utils.to_categorical(y_test, CLASS_NUM)
 
     return x_train, x_test, y_train, y_test, input_shape
+
+def load_images_from_folder(folder):
+    all_images = []
+    image_list = os.listdir(folder)
+    image_list.sort(key=lambda x: int(os.path.splitext(x)[0]))
+    for image_path in image_list:
+        img = io.imread(os.path.join(folder,image_path))
+        img = util.invert(img)
+        all_images.append(img)
+    return np.array(all_images)
+
+def read_dataset_image_cnn(CLASS_NUM):
+    # Load mnist data set
+    x_test_image = load_images_from_folder('test_images')
+    num_images = x_test_image.shape[0]
+    y_test_image = np.arange(0, num_images)
+
+    WIDTH = x_test_image.shape[2]
+    HEIGHT = x_test_image.shape[1]
+    CLASS_NUM = 10
+
+    # case by backend, 
+    if K.image_data_format() == 'channels_first':
+        #[channel, height, width]
+        x_test_image = x_test_image.reshape(x_test_image.shape[0], 1, HEIGHT, WIDTH)
+        input_shape = (1, HEIGHT, WIDTH)
+    else:
+        #[height, width, channel]
+        x_test_image = x_test_image.reshape(x_test_image.shape[0], HEIGHT, WIDTH, 1)
+        input_shape = (HEIGHT, WIDTH, 1)
+
+    # image normalization
+    x_test_image = x_test_image.astype('float32')
+    x_test_image /= 255
+
+    # convert class with one-hot encoding
+    # 5 -> [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
+    y_test_image = keras.utils.to_categorical(y_test_image, CLASS_NUM)
+
+    return x_test_image, y_test_image, input_shape
+
+def read_dataset_image_dense(CLASS_NUM):
+    # Load mnist data set
+    x_test_image = load_images_from_folder('test_images')
+    num_images = x_test_image.shape[0]
+    y_test_image = np.arange(0, num_images)
+
+    WIDTH = x_test_image.shape[2]
+    HEIGHT = x_test_image.shape[1]
+    CLASS_NUM = 10
+
+    x_test_image = x_test_image.reshape(x_test_image.shape[0], HEIGHT*WIDTH)
+    input_shape = [HEIGHT*WIDTH]
+
+    # image normalization
+    x_test_image = x_test_image.astype('float32')
+    x_test_image /= 255
+
+    # convert class with one-hot encoding
+    # 5 -> [0. 0. 0. 0. 0. 1. 0. 0. 0. 0.]
+    y_test_image = keras.utils.to_categorical(y_test_image, CLASS_NUM)
+
+    return x_test_image, y_test_image, input_shape
 
 def mnist_cnn_model(input_shape, CLASS_NUM):
     model = Sequential()
